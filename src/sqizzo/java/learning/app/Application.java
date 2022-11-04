28eb21fb6ddfd05d3023a1300796bc0ceb9051e1
baihaqi2193 <baihaqi2193@gmail.com>
2022-11-04 19:56:54 +0700

    Added access modifier lesson

---
+++ src/sqizzo/java/learning/app/Application.java
@@ -0,0 +1,17 @@
+package sqizzo.java.learning.app;
+
+// import  java.lang.*;
+// Default import for java language
+import  sqizzo.java.learning.data.*;
+// import sqizzo.java.learning.data.Product;
+// import sqizzo.java.learning.data.ProductApp;
+
+public class Application {
+    public static void main(String[] args) {
+        Product product = new Product("Macbook Pro", 30000000);
+        System.out.println(product);
+
+//        System.out.println(product.name);
+//        Error karena protected
+    }
+}
